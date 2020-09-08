7eaefa7b8dca156399fe2b9b010dde8ea14a0beb
camposweb <camposweb@gmail.com>
2020-09-08 01:16:18 -0300

    Difinindo fim do jogo

--- js/main.js
+++ js/main.js
@@ -48,7 +48,6 @@ function update(event){
 }
 
 function iniciarJogo(){
-
     if (snake[0].x > 15 * box && direction == 'right') {
         snake[0].x = 0;
     }
@@ -60,6 +59,13 @@ function iniciarJogo(){
     }
     if (snake[0].y > 15 * box && direction == 'down') {
         snake[0].y = 0;
+    }
+
+    for(let i = 1; i < snake.length; i++){
+        if(snake[0].x == snake[i].x && snake[0].y == snake[i].y){
+            clearInterval(jogo);
+            alert('Game Over :( ');
+        }
     }
 
     criarBG();
